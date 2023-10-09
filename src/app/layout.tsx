b90ee3e137481442ca312c8caeec8fff8d0a6dbf
Bob_Smith <reversetype@gmail.com>
2023-10-09 11:38:34 -0400

    🔧 chore(layout.tsx): import GTM_ID from "@/lib/gtm" 🔧 chore(layout.tsx): add Google Tag Manager script 🔧 chore(Footer.jsx): update footer layout and legal links The layout.tsx file now imports the GTM_ID from the "@/lib/gtm" module. This change is a chore as it only updates the import statement.
    The layout.tsx file also adds a Google Tag Manager script using the Script component from the "next/script" package. This script is added to the head of the document and initializes the Google Tag Manager.
    
    The Footer.jsx file updates the layout of the footer and adds legal links. The links include "About Us", "Terms and Conditions", "Privacy Policy", "California Privacy Notice", "Do Not Sell or Share My Personal Information", "Data Attribution", "Personal Information Protection", and "Privacy Preferences". The footer layout is updated to include the Appily logo, legal links,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GTM_ID } from "@/lib/gtm";
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import React from "react";
@@ -33,6 +34,19 @@ export default async function RootLayout({
 					}}
 				/>
 				{/* <!-- OneTrust Cookies Consent Notice end for appily.com --> */}
+				<Script
+					id="gtag-base"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+					}}
+				/>
 			</head>
 			<body>
 				<Providers>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,66 +1,105 @@
-"use client";
-
 import Image from "next/image";
-import React from "react";
-import { MdHelpOutline, MdOutlinePrivacyTip } from "react-icons/md";
-import { Reoverlay } from "reoverlay";
+import styles from "../styles/modules/PageFooter.module.scss";
 
-import { HelpModal, PrivacyModal } from "@/components/Modals";
-import styles from "@/styles/components/PageFooter.module.scss";
-
-const Footer = () => {
-	const helpClick = () => {
-		Reoverlay.showModal(HelpModal, {});
-	};
-	const privacyClick = () => {
-		if (
-			typeof window.OneTrust === "undefined" ||
-			typeof window.OneTrust.ToggleInfoDisplay !== "function"
-		) {
-			return;
-		}
-		window.OneTrust.ToggleInfoDisplay();
-	};
+export default function PageFooter() {
+	const currentYear = new Date().getFullYear();
 
 	return (
-		<footer className={styles.pageFooter}>
+		<footer className={styles["page-footer"]}>
 			<div className={styles.wrapper}>
-				<figure className={styles.logo}>
-					<Image src="/images/appily-logo.svg" alt="Appily Logo" fill />
-				</figure>
-				<div className="copyright">
-					<p>
-						© {new Date().getFullYear()} All rights reserved.{" "}
-						<a href="https://www.appily.com">www.Appily.com</a>
-					</p>
-				</div>
-				<div className="help-privacy">
-					<ul>
-						<li>
-							<button
-								type="button"
-								className={styles.helpPrivBtn}
-								onClick={helpClick}
-							>
-								<MdHelpOutline />
-								Help
-							</button>
-						</li>
-						<li>
-							<button
-								type="button"
-								className={styles.helpPrivBtn}
-								onClick={privacyClick}
-							>
-								<MdOutlinePrivacyTip />
-								Privacy
-							</button>
-						</li>
-					</ul>
+				<div className={styles.logoLegal}>
+					<figure className={styles.logo}>
+						<Image src="/images/appily-logo-dark.svg" alt="Appily Logo" fill />
+					</figure>
+					<div className={styles.legalLinks}>
+						<ul>
+							<li>
+								<a
+									href="https://www.appily.com/about"
+									target="_blank"
+									rel="noreferrer"
+								>
+									About Us
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://www.appily.com/terms-and-conditions"
+									target="_blank"
+									rel="noreferrer"
+								>
+									Terms and Conditions
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://www.appily.com/privacy-policy"
+									target="_blank"
+									rel="noreferrer"
+								>
+									Privacy Policy
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://privacy.eab.com/appily-cs-tours-CA"
+									target="_blank"
+									rel="noreferrer"
+								>
+									California Privacy Notice
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://www.appily.com/your-privacy-rights"
+									target="_blank"
+									rel="noreferrer"
+								>
+									Do Not Sell or Share My Personal Information
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://www.appily.com/data"
+									target="_blank"
+									rel="noreferrer"
+								>
+									Data Attribution
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://www.appily.com/your-privacy-rights"
+									target="_blank"
+									rel="noreferrer"
+								>
+									Personal Information Protection
+								</a>
+							</li>
+							<li>
+								<a
+									href="#"
+									rel="noreferrer"
+									onClick={e => {
+										e.preventDefault();
+										window.OneTrust.ToggleInfoDisplay();
+									}}
+								>
+									Privacy Preferences
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div className="copyright">
+						<p>
+							© {currentYear} All rights reserved.{" "}
+							<a href="https://www.appily.com" target="_blank" rel="noreferrer">
+								www.Appily.com
+							</a>
+						</p>
+					</div>
 				</div>
 			</div>
 		</footer>
 	);
-};
-
-export default Footer;
+}
