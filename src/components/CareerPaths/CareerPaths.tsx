6992cb355d159801f0a4915bca662c92923c0db8
Andrei Olea <aolea@eab.com>
2023-10-11 13:04:26 -0400

    🐛 fix(CareerPaths.tsx): fix indentation and add missing className ✨ feat(CareerPaths.tsx): update CareerPaths component to include section and div elements 🐛 fix(TextWithImage.tsx): fix indentation and add missing className ✨ feat(TextWithImage.tsx): update TextWithImage component to include section and div elements 🐛 fix(results-creative-marketer.json): fix formatting of description in careerPaths object 🐛 fix(_fonts.scss): remove commented out code 🐛 fix(_themes.scss): fix color values in violet theme ✨ feat(_themes.scss): add new background and shadow colors to themes ✨ feat(_resultsPage.scss): add styles for CareerPaths and TextWithImage components

--- src/components/CareerPaths/CareerPaths.tsx
+++ src/components/CareerPaths/CareerPaths.tsx
@@ -1,39 +1,52 @@
 import Image from "next/image";
 
 interface CareerPath {
-	imagePath: string;
-	title: string;
-	description: string;
+  imagePath: string;
+  title: string;
+  description: string;
 }
 
 interface CareerPathsProps {
-	careerPaths: {
-		title: string;
-		description: string;
-		paths: CareerPath[];
-	};
+  careerPaths: {
+    title: string;
+    description: string;
+    paths: CareerPath[];
+  };
 }
 
 export function CareerPaths({ careerPaths }: CareerPathsProps) {
-	const { title, description, paths } = careerPaths;
-	return (
-		<>
-			<h2>{title}</h2>
-			<p>{description}</p>
-			<ul>
-				{paths.map(path => (
-					<li key={path.title}>
-						<Image
-							src={path.imagePath}
-							width={340}
-							height={240}
-							alt={path.title}
-						/>
-						<h3>{path.title}</h3>
-						<p>{path.description}</p>
-					</li>
-				))}
-			</ul>
-		</>
-	);
+  const { title, description, paths } = careerPaths;
+  return (
+    <section className="careerPaths">
+      <div className="group column">
+        <div className="intro-text">
+          <h2>{title}</h2>
+          {/* <p>{description}</p> */}
+          <p
+            dangerouslySetInnerHTML={{
+              __html: description,
+            }}
+          />
+        </div>
+        <ul className="thePaths">
+          {paths.map(path => (
+            <li key={path.title}>
+              <figure>
+                <Image
+                  src={path.imagePath}
+                  width={340}
+                  height={240}
+                  alt={path.title}
+                />
+              </figure>
+              <div className="careerPathContent">
+                <h3>{path.title}</h3>
+                <p>{path.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
