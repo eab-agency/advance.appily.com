f23bd0516029c9983e42550d2f9fb95a97a17050
Andrei Olea <aolea@eab.com>
2023-11-02 12:35:42 -0400

    appily team comments, titlecase, image replacement, image quality enhancement, footer logo changed

--- src/components/TextWithImage/TextWithImage.tsx
+++ src/components/TextWithImage/TextWithImage.tsx
@@ -5,19 +5,20 @@ type InnerHTMLComponentProps = {
   content: string;
   imagePath?: string;
   className?: string;
+  altText?: string;
 };
 
 export const TextWithImage: React.FC<InnerHTMLComponentProps> = ({
   content,
   imagePath,
   className,
+  altText,
 }) => {
   return (
     <section className={`textWithImage ${className}`}>
       <div className="group cols-2">
         <figure className="highlighted-img">
-
-          {imagePath && <Image src={imagePath} height={300} width={250} alt="" />}
+          {imagePath && <Image src={imagePath} height={800} width={800} alt={altText ?? ''} />}
         </figure>
         <div className="textContainer column" dangerouslySetInnerHTML={{ __html: content }} />
       </div>
