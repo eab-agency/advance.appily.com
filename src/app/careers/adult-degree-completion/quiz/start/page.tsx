4ee3ada2b1ec91445c18cf45c0524ee0f4efc0e8
Andrei Olea <aolea@eab.com>
2023-11-08 09:12:56 -0500

    initial commit

---
+++ src/app/careers/adult-degree-completion/quiz/start/page.tsx
@@ -0,0 +1,38 @@
+import { Quiz } from "@/components";
+import { fetchQuizData } from "@/lib/fetchQuizData";
+
+import { mergeOpenGraph, mergeTwitter } from "@/seo";
+import { Metadata } from "next";
+
+const title = "Forge Your Path in Business";
+const description = "Start the Appily Business Quiz Now";
+
+export const metadata: Metadata = {
+  title: title,
+  description: description,
+  openGraph: mergeOpenGraph({
+    title: title,
+    description: description,
+  }),
+  twitter: mergeTwitter({
+    title: title,
+    description: description,
+  }),
+};
+
+const VERTICAL = "business";
+async function QuizHome() {
+  const quizData = await fetchQuizData(VERTICAL);
+
+  return (
+    quizData && (
+      <Quiz
+        vertical={VERTICAL}
+        quizData={quizData}
+        resultsFormId="6"
+        title={title}
+      />
+    )
+  );
+}
+export default QuizHome;
