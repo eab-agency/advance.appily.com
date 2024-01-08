c3dd859032c1f5a724b0848010c7323b199f29c1
Bob_Smith <reversetype@gmail.com>
2024-01-08 13:13:47 -0500

    🚀 feat(layout.tsx): add LocationInDevMode component to display and change location in development mode 🔧 chore(PageLayout.js): remove LocationInDevMode component from PageLayout 🔧 chore(getMatchedSchool.ts): export statesToMatchAgainst object for use in other files The LocationInDevMode component is added to the layout.tsx file to display the current location and allow changing the location in development mode. This component provides a dropdown menu to select a state and updates the location based on the selected state. The PageLayout.js file no longer includes the LocationInDevMode component. The statesToMatchAgainst object is now exported from the getMatchedSchool.ts file for use in other files.

---
+++ src/components/PageLayouts/LocationInDevMode.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useUser } from "@/context/context";
+import { statesToMatchAgainst } from "@/helpers/getMatchedSchool";
+import isDevMode from "@/helpers/isDevMode";
+
+const LocationInDevMode = () => {
+	const { location, setLocation } = useUser();
+
+	const setInputLocation = selectedLocation => {
+		setLocation({ region_iso_code: selectedLocation });
+	};
+	// only return if isDevMode
+	if (!isDevMode()) {
+		return null;
+	}
+
+	const styles = {
+		position: "fixed",
+		top: "0",
+		left: "0",
+		zIndex: "9999",
+		backgroundColor: "white",
+		padding: "1rem",
+	};
+
+	return (
+		<div style={styles}>
+			{location ? (
+				<>
+					<p>Region: {location.region_iso_code}</p>
+					<p>Country: {location.country_code}</p>
+					<p>Not US: {location.notUS ? "true" : "false"}</p>
+				</>
+			) : (
+				<p>Location: loading...</p>
+			)}
+			<label htmlFor="location">Change Location:</label>
+			<select id="location" onChange={e => setInputLocation(e.target.value)}>
+				<option value="">Select a state</option>
+				{/* iterated through the statesToMatchAgainst object for each option */}
+				{Object.keys(statesToMatchAgainst).map(state => (
+					<option key={state} value={state}>
+						{state}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
+
+export default LocationInDevMode;
