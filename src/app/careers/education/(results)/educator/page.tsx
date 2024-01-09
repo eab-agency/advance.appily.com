d144ee1dc1bbd06097fded92e96fd261fb7e501f
Andrei Olea <aolea@eab.com>
2024-01-09 00:17:21 -0500

    🐛 fix(page.tsx): fix indentation and add dangerouslySetInnerHTML for detailedDescription ✨ feat(educator.json): update title and detailedDescription for The Educator career path

--- src/app/careers/education/(results)/educator/page.tsx
+++ src/app/careers/education/(results)/educator/page.tsx
@@ -6,16 +6,16 @@ import { Suspense } from "react";
 import { useEffect, useRef } from "react";
 
 import {
-	CareerPaths,
-	CarouselWithForm,
-	ChoosingRightSchoolBusinessDegree,
-	KeepExploring,
-	Stats,
-	StickyCta,
-	SubNav,
-	Tabs,
-	TextWithImage,
-	WhatDegrees,
+  CareerPaths,
+  CarouselWithForm,
+  ChoosingRightSchoolBusinessDegree,
+  KeepExploring,
+  Stats,
+  StickyCta,
+  SubNav,
+  Tabs,
+  TextWithImage,
+  WhatDegrees,
 } from "@/components";
 
 import { useUser } from "@/context/context";
@@ -23,58 +23,62 @@ import dataLinks from "@/data/links-education.json";
 import data from "@/data/results/education/educator.json";
 
 function TabsFallback() {
-	return <>Tabs loading...</>;
+  return <>Tabs loading...</>;
 }
 
 export default function Page() {
-	const carouselRef = useRef(null);
-	const { results: links } = dataLinks;
-	const { setVertical, vertical } = useUser();
-	useEffect(() => {
-		setVertical("Education");
-	}, []);
-	return (
-		<>
-			<div className="resultContent">
-				<section className="resultsHero">
-					<div className="group">
-						<div className="heroContent column">
-							<div className="intro-title">
-								<span>Your ideal role could be ...</span>
-							</div>
-							<h1>{data.title}</h1>
-							<p>{data.detailedDescription}</p>
-						</div>
-						<figure className="column">
-							<Lottie animationData={creativeMarketer} loop={true} />
-						</figure>
-					</div>
-				</section>
+  const carouselRef = useRef(null);
+  const { results: links } = dataLinks;
+  const { setVertical, vertical } = useUser();
+  useEffect(() => {
+    setVertical("Education");
+  }, []);
+  return (
+    <>
+      <div className="resultContent">
+        <section className="resultsHero">
+          <div className="group">
+            <div className="heroContent column">
+              <div className="intro-title">
+                <span>Your ideal role could be ...</span>
+              </div>
+              <h1>{data.title}</h1>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: data.detailedDescription,
+                }}
+              />
+            </div>
+            <figure className="column">
+              <Lottie animationData={creativeMarketer} loop={true} />
+            </figure>
+          </div>
+        </section>
 
-				{links && <SubNav links={links} />}
+        {links && <SubNav links={links} />}
 
-				<Suspense fallback={<TabsFallback />}>
-					<Tabs className="react-tabs" tabs={data.tabs} />
-				</Suspense>
+        <Suspense fallback={<TabsFallback />}>
+          <Tabs className="react-tabs" tabs={data.tabs} />
+        </Suspense>
 
-				<Stats stats={data.stats} source={data.statsSource} />
+        <Stats stats={data.stats} source={data.statsSource} />
 
-				{/* <WhatDegrees whatDegreesData={data.degreeTabs} /> */}
+        {/* <WhatDegrees whatDegreesData={data.degreeTabs} /> */}
 
-				<div
-					id="explore-your-school-matches"
-					className="carouselWithForm"
-					ref={carouselRef}
-				>
-					<CarouselWithForm formId="7" collectData={false} />
-				</div>
+        <div
+          id="explore-your-school-matches"
+          className="carouselWithForm"
+          ref={carouselRef}
+        >
+          <CarouselWithForm formId="7" collectData={false} />
+        </div>
 
-				<ChoosingRightSchoolBusinessDegree />
+        <ChoosingRightSchoolBusinessDegree />
 
-				<KeepExploring trackedElement={carouselRef} />
-			</div>
+        <KeepExploring trackedElement={carouselRef} />
+      </div>
 
-			<StickyCta trackedElement={carouselRef} />
-		</>
-	);
+      <StickyCta trackedElement={carouselRef} />
+    </>
+  );
 }
