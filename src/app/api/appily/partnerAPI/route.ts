6375e5b4e2a950330b9045de0a937a106005e0b3
Smith, Bob <bobsmith@eab.com>
2024-01-19 22:14:11 +0000

    education vertical

--- src/app/api/appily/partnerAPI/route.ts
+++ src/app/api/appily/partnerAPI/route.ts
@@ -1,16 +1,16 @@
 import { formatFormDataForAppilyPartnerAPI } from "@/lib/formatFormDataForAppilyPartnerAPI";
+import { ConsoleLogger } from "aws-amplify/utils";
+const logger = new ConsoleLogger("partnerAPI/route.ts");
 
 const appilyAPI =
 	process.env.NODE_ENV === "development"
 		? process.env.DEV_APPILY_API_URL
 		: process.env.APPILY_API_URL || "";
-// console.log("🚀 ~ file: route.ts:4 ~ appilyAPI:", appilyAPI);
 
 const partnerKey =
 	process.env.NODE_ENV === "development"
 		? process.env.DEV_APPILY_API_KEY
 		: process.env.APPILY_API_KEY;
-// console.log("🚀 ~ file: route.ts:9 ~ partnerKey:", partnerKey);
 
 const partnerExternalId = process.env.PARTNER_EXTERNAL_ID;
 // console.log("🚀 ~ file: route.ts:14 ~ partnerExternalId:", partnerExternalId);
@@ -18,19 +18,26 @@ const programExternalId = process.env.PROGRAM_EXTERNAL_ID;
 // console.log("🚀 ~ file: route.ts:16 ~ programExternalId:", programExternalId);
 
 export async function POST(request: Request) {
+	logger.error("🚀 ~ Submitting data to:", appilyAPI);
+	// logger.error("🚀 ~ partnerKey:", partnerKey);
+
 	try {
 		const res = await request.json();
 
 		const formattedData = formatFormDataForAppilyPartnerAPI(res);
 		formattedData.partnerExternalId = partnerExternalId || "";
+
 		formattedData.programExternalId = programExternalId || "";
 		formattedData.partnerKey = partnerKey || "";
+
 		if (process.env.NODE_ENV === "development") {
-			console.log(
-				"🚀 ~ file: route.ts:25 ~ POST ~ formattedData in development mode:",
-				JSON.stringify(formattedData),
-			);
+			// console.log(
+			// 	"🚀 ~ file: route.ts:25 ~ POST ~ formattedData in development mode:",
+			// 	JSON.stringify(formattedData),
+			// );
 		}
+
+		// logger.error("info bar: formattedData", JSON.stringify(formattedData));
 
 		const responseFromAppily = await fetch(
 			`${appilyAPI}/partner/v1/register-student/adult-learner`,
@@ -44,25 +51,43 @@ export async function POST(request: Request) {
 		);
 
 		if (responseFromAppily.status !== 200) {
+			logger.error(
+				"🚨🚨🚨 responseFromAppily.headers",
+				responseFromAppily.headers,
+			);
+			logger.error(
+				"🚨🚨🚨 responseFromAppily.statusText",
+				responseFromAppily.statusText,
+			);
+			logger.error(
+				"🚨🚨🚨 responseFromAppily.status",
+				responseFromAppily.status,
+			);
+			// console.log("🚨🚨🚨 ~ POST ~ responseFromAppily:", responseFromAppily);
 			throw new Error(
 				`HTTP request failed with status ${responseFromAppily.status}`,
 			);
 		}
 
 		const data = await responseFromAppily.json();
+		// console.log("🚀 ~ POST ~ data response:", data);
+		// logger.error("info bar: data response", data);
 
 		if (data.meta && data.meta.success === false) {
 			const errorResponse = {
 				error: data.meta.error,
 				validationMessages: data.meta.validationMessages,
 			};
-			console.error("🚨🚨 Error: ", errorResponse);
+			logger.error("error bar", errorResponse);
+			// console.error("🚨🚨 Error: ", errorResponse);
 			return Response.json({ errorResponse });
 		} else {
+			logger.error("successful data: ", data);
 			return Response.json({ data });
 		}
 	} catch (error) {
 		console.error("🚨🚨🚨🚨 Error: ", error);
+		logger.error("error bar", error);
 		return Response.json({ error: "An error occurred" });
 	}
 }
---
+++ src/app/careers/education/page.tsx
@@ -0,0 +1,214 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+
+import {
+  Accordion,
+  Button,
+  CarouselWithForm,
+  Header,
+  Stats,
+  StickyCta,
+  Testimonial,
+  WhatIsCappex,
+} from "@/components";
+import { IconCard } from "@/components/IconCard/IconCard";
+import { useUser } from "@/context/context";
+import data from "@/data/careers-education.json";
+import Link from "next/link";
+
+/* eslint-disable react/no-danger */
+const BusinessSeoPage = () => {
+  const { setVertical } = useUser();
+  useEffect(() => {
+    setVertical("Education");
+  }, []);
+  const reasonsArray = data.whyChoose.reasons;
+  const rightCareerArray = data.rightCareer[0].reasons;
+  const rightCareerList = rightCareerArray.map((reason, index) => (
+    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+    <li key={index}>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: reason.description,
+        }}
+      />
+    </li>
+  ));
+
+  const carouselRef = useRef(null);
+
+  const [themeMode, setThemeMode] = useState("light");
+
+  useEffect(() => {
+    if (window.matchMedia?.("(prefers-color-scheme: dark)").matches) {
+      setThemeMode("dark");
+    } else {
+      setThemeMode("light");
+    }
+  }, []);
+
+  return (
+    <>
+      <section className="pageHero">
+        <div className="group center-aligned center-justified">
+          <h1
+            dangerouslySetInnerHTML={{
+              __html: data.pageTitle,
+            }}
+          />
+          <figure className="heroImage">
+            <Image
+              src="/images/education/education-career-hero.jpg"
+              width={600}
+              height={600}
+              alt="Man teaching a class"
+            />
+          </figure>
+        </div>
+      </section>
+      <section className="quizSection">
+        <div className="group center-aligned row cols-2">
+          <div className="column">
+            <figure className="highlighted-img">
+              <Image
+                src="/images/education/education-career-quiz.webp"
+                width={480}
+                height={480}
+                alt="Teacher at the front of a classroom"
+              />
+            </figure>
+          </div>
+          <div className="column intro-text">
+            <h2>{data.quizSection.title}</h2>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: data.quizSection.content,
+              }}
+            />
+            <Button
+              appearance="primary"
+              label={data.quizSection.buttonText}
+              href="/careers/education/quiz"
+              className="button btn-primary btn-click-quiz"
+            />
+          </div>
+        </div>
+      </section>
+
+      <Stats
+        stats={data.stats}
+        source={data.statsSource}
+        className="stats-section"
+      />
+
+      <section className="whyChoose">
+        <div className="group center-aligned cols-2">
+          <div className="column">
+            <div className="intro">
+              <h2>{data.whyChoose.title}</h2>
+              <h2>
+                <strong>{data.whyChoose.subTitle}</strong>
+              </h2>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: data.whyChoose.description,
+                }}
+              />
+            </div>
+            <div className="accordion-group">
+              {reasonsArray.map((reason, index) => (
+                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                <Accordion key={index} title={reason.title}>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: reason.description,
+                    }}
+                  />
+                </Accordion>
+              ))}
+            </div>
+            <p>
+              <Link href="/careers/education/quia">
+                Take the Education Career Quiz
+              </Link>{" "}
+              to see if your skills and interests make you a great fit for a
+              classroom setting!
+            </p>
+          </div>
+          <div className="column">
+            <figure className="highlighted-img">
+              <Image
+                src="/images/education-why-become-a-teacher.jpg"
+                width={480}
+                height={480}
+                alt="Man helping a student with a computer program"
+              />
+            </figure>
+          </div>
+        </div>
+      </section>
+
+      <Testimonial testimonialData={data.testimonial} />
+
+      <section className="rightCareer">
+        <div className="group column center-aligned">
+          <div className="column">
+            <h2>
+              What are Some Good Jobs for <strong>Former Teachers?</strong>
+            </h2>
+            <p>{data.rightCareer[0].description}</p>
+          </div>
+          <div className="accordion-group">
+            {data.rightCareer[0].reasons.map((reason, index) => (
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              <Accordion key={index} title={reason.title}>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: reason.description,
+                  }}
+                />
+              </Accordion>
+            ))}
+          </div>
+          <p
+            dangerouslySetInnerHTML={{ __html: data.rightCareer[0].endCopy }}
+          />
+        </div>
+      </section>
+      {/* <div id="explore-your-school-matches" ref={carouselRef}>
+				<CarouselWithForm formId="8" />
+			</div> */}
+
+      <WhatIsCappex />
+
+      <section className="takeQuiz full-content">
+        <div className="group row">
+          <div className="column">
+            <figure>
+              <Image
+                src="/images/ready-to-find-your-role.jpg"
+                width={900}
+                height={900}
+                alt="Take our free quiz"
+              />
+            </figure>
+          </div>
+          <div className="content column">
+            <h2>{data.takeQuiz.title}</h2>
+            <p>{data.takeQuiz.description}</p>
+            <Button
+              type="button"
+              label={data.takeQuiz.buttonText}
+              href="/careers/education/quiz"
+              className="button btn-primary btn-click-quiz"
+            />
+          </div>
+        </div>
+      </section>
+      <StickyCta />
+    </>
+  );
+};
+
+export default BusinessSeoPage;
