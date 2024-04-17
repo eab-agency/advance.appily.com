97644cba2547a523b6f21d8ab98b68eac5abcf98
Smith, Bob <bobsmith@eab.com>
2024-04-17 14:16:39 +0000

    🔧 update to use next third party for GTM

--- src/components/Heros/PageHero.tsx
+++ src/components/Heros/PageHero.tsx
@@ -1,30 +1,28 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface PageHeroProps {
   image: {
-    src: string
-    alt: string
-  }
-  children?: React.ReactNode
+    src: string;
+    alt: string;
+  };
+  children?: React.ReactNode;
 }
 
 export default function PageHero({ image, children }: PageHeroProps) {
   return (
     <section className="pageHero">
       <div className="group center-aligned center-justified">
-        <div className="content-container">
-          {children}
-        </div>
-        <figure className="heroImage">
+        <div className="content-container">{children}</div>
+        <figure className="heroImage" style={{ height: 672, width: 672 }}>
           <Image
             src={image.src}
-            width={1200}
-            height={1200}
+            width={672}
+            height={672}
             alt={image.alt}
             priority={true}
           />
         </figure>
       </div>
     </section>
-  )
+  );
 }
