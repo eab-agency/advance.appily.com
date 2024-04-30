14b294a87e979a42f3611cfee3c6c0fbdda0a849
Smith, Bob <bobsmith@eab.com>
2024-04-30 14:58:38 +0000

    ✨ move ADC build plan quiz to 25% to flex reg

--- src/app/degree-completion/build-plan-quiz/page.tsx
+++ src/app/degree-completion/build-plan-quiz/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components";
 import Image from "next/image";
+import RandomComponent from "@/hooks/useRandomComponent";
+import ABButton from "@/components/Button/ABButton";
 // eslint-disable-next-line import/no-unresolved
 
 import { mergeOpenGraph, mergeTwitter } from "@/seo";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 const title = "Back-to-School Plan";
-const description = "Answer a few questions and we'll help you build a plan to go back to school, including how to pay for it and what degrees can prepare you for in-demand careers.";
+const description =
+  "Answer a few questions and we'll help you build a plan to go back to school, including how to pay for it and what degrees can prepare you for in-demand careers.";
 
 export const metadata: Metadata = {
   title: title,
@@ -22,6 +26,23 @@ export const metadata: Metadata = {
 };
 
 export default function QuizLandingPage() {
+  const ButtonOne = (
+    <ABButton
+      label="Answer a Few Questions +"
+      appearance="primary"
+      href="https://my.appily.com/register/adult/buildplanquiz1"
+      className="button btn-primary"
+    />
+  );
+
+  const ButtonTwo = (
+    <ABButton
+      label="Answer a Few Questions"
+      appearance="primary"
+      href="/degree-completion/build-plan-quiz/start"
+      className="button btn-primary"
+    />
+  );
   return (
     <>
       <div className="quiz-wrapper">
@@ -34,23 +55,36 @@ export default function QuizLandingPage() {
             </header>
             <p>
               Stepping back into the world of education might seem daunting, but
-              remember, <strong>every great journey begins with a single step</strong>—and you&apos;re
-              already on your way.
+              remember,{" "}
+              <strong>every great journey begins with a single step</strong>—and
+              you&apos;re already on your way.
             </p>
             <p>Your plan will include:</p>
             <ul>
-              <li>Information about the <strong>lifetime value</strong> of a bachelor&apos;s degree</li>
-              <li>Tips for <strong>reducing cost</strong> and funding your degree</li>
-              <li>In-demand <strong>career paths</strong> in your area of interest</li>
-              <li><strong>Degrees and schools</strong> that can help you reach your goals</li>
-              <li>Detailed <strong>next steps</strong> you can take</li>
+              <li>
+                Information about the <strong>lifetime value</strong> of a
+                bachelor&apos;s degree
+              </li>
+              <li>
+                Tips for <strong>reducing cost</strong> and funding your degree
+              </li>
+              <li>
+                In-demand <strong>career paths</strong> in your area of interest
+              </li>
+              <li>
+                <strong>Degrees and schools</strong> that can help you reach
+                your goals
+              </li>
+              <li>
+                Detailed <strong>next steps</strong> you can take
+              </li>
             </ul>
-            <Button
-              label="Answer a Few Questions"
-              appearance="primary"
-              href="/degree-completion/build-plan-quiz/start"
-              className="button btn-primary"
-            />
+            <Suspense>
+              <RandomComponent
+                PercentageComponent={ButtonOne}
+                FallBackComponent={ButtonTwo}
+              />
+            </Suspense>
           </div>
           <figure className="column highlighted-img">
             <Image
