4462fa00f02da419538ae4ce29e506c3d380bfe4
Bob Smith <reversetype@gmail.com>
2024-06-11 16:22:02 -0400

    chore: (performance) if in dev mode, do not load GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ import Script from "next/script";
 import HeaderComponent from "../components/Header";
 import FooterComponent from "../components/Footer";
 
-const isDev = process.env.NEXT_PUBLIC_ISDEV;
+import isDev from "@/helpers/isDevMode";
 
 export default async function RootLayout({
   children,
@@ -26,20 +26,26 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://use.typekit.net" />
         <link rel="stylesheet" href="https://use.typekit.net/wba2ytz.css" />
         {/* <!-- OneTrust Cookies Consent Notice start for appily.com --> */}
-        <Script src="https://cdn.cookielaw.org/consent/f621c13f-1c94-43c9-8362-0f5d72c69f26/OtAutoBlock.js" />
-        <Script
-          src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
-          data-domain-script="f621c13f-1c94-43c9-8362-0f5d72c69f26"
-        />
-        <Script
-          id="otStubData"
-          dangerouslySetInnerHTML={{
-            __html: "function OptanonWrapper() {}",
-          }}
-        />
+        {!isDev && (
+          <>
+            <Script src="https://cdn.cookielaw.org/consent/f621c13f-1c94-43c9-8362-0f5d72c69f26/OtAutoBlock.js" />
+            <Script
+              src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
+              data-domain-script="f621c13f-1c94-43c9-8362-0f5d72c69f26"
+            />
+
+            <Script
+              id="otStubData"
+              dangerouslySetInnerHTML={{
+                __html: "function OptanonWrapper() {}",
+              }}
+            />
+          </>
+        )}
         {/* <!-- OneTrust Cookies Consent Notice end for appily.com --> */}
       </head>
-      <GoogleTagManager gtmId={GTM_ID} />
+      {/* if isDev is false then load GoogleTagManager */}
+      {!isDev && <GoogleTagManager gtmId={GTM_ID} />}
       <body>
         <Providers>
           {/* @ts-expect-error Server Component */}
