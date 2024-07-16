13979ff948ca509f7b3543480333509f7ce3250a
Olea, Andrei <aolea@eab.com>
2024-07-16 18:09:41 +0000

    🔨 refactor(payload-types.ts): restructure sectionHead interface to include...

--- src/blocks/TabSection/index.tsx
+++ src/blocks/TabSection/index.tsx
@@ -1,8 +1,12 @@
-import { Suspense } from "react";
-import TabsComponent from "./TabsComponent";
 import "@/styles/layouts/TabSection.scss";
 import { sectionClassNames, SectionClassNamesProps } from "@/utilities/sectionClassNames";
-import RichText from "@/components/RichText";
+import { Fragment, Suspense } from "react";
+import TabsComponent from "./TabsComponent";
+
+import { Media } from '@/components/Media';
+import RichText from '@/components/RichText';
+import AccordionSection from '@/components/commonComponent/AccordionGroup';
+import ButtonGroup from '@/components/commonComponent/ButtonGroup';
 
 interface TabsSectionProps {
   tabs: {
@@ -10,34 +14,102 @@ interface TabsSectionProps {
   }[];
   className?: string;
   tabSectionBackgroundColor?: string;
-  sectionHead?: boolean;
-  sectionIntro?: string;
+  sectionHead: {
+    showSectionHead: boolean;
+    headContent: {
+      blocks: {
+        blockType: string;
+        richText?: string;
+        buttonGroup?: {
+          buttons: {
+            text: string;
+            link: string;
+            type: string;
+          }[];
+        }
+      }[];
+    }
+  };
+  sectionFooter: {
+    showSectionFooter: boolean;
+    footerContent: {
+      blocks: {
+        blockType: string;
+        richText?: string;
+        buttonGroup?: {
+          buttons: {
+            text: string;
+            link: string;
+            type: string;
+          }[];
+        }
+      }[];
+    }
+  };
 }
 
 function TabsFallback() {
   return <>Tabs loading...</>;
 }
 
+const blockRenderers = {
+  richText: (block) => <RichText content={block.richText} />,
+  mediaBlock: (block) => {
+    const { media, cornerStyle, enableHighlight } = block;
+    return (
+      <Media
+        resource={media}
+        cornerStyle={cornerStyle}
+        enableHighlight={enableHighlight}
+        priority
+      />
+    )
+  },
+  accordion: (block) => <AccordionSection data={block} />,
+  ButtonGroup: (block) => <ButtonGroup data={block} />,
+};
+
 
 export default function TabsSection({
   tabs,
   tabSectionBackgroundColor,
   sectionHead,
-  sectionIntro,
+  sectionFooter,
 }: TabsSectionProps) {
   const sectionClassProps: SectionClassNamesProps = { backgroundColor: tabSectionBackgroundColor };
 
+  const { headContent, showSectionHead } = sectionHead;
+  const { footerContent, showSectionFooter } = sectionFooter;
+
   return (
     <section className={`tabsSection column ${sectionClassNames(sectionClassProps)}`}>
-      {sectionHead && (
-        <header className="center-aligned center-justified">
-          <RichText content={sectionIntro} />
+      {showSectionHead && (
+        <header className="tabsSection__head">
+          {headContent.blocks?.map((block, blockIndex) => {
+            return (
+              <Fragment key={blockIndex}>
+                {blockRenderers[block.blockType](block)}
+              </Fragment>
+            );
+          })}
         </header>
       )}
 
       <Suspense fallback={<TabsFallback />}>
-      <TabsComponent className="react-tabs" tabs={tabs} id={!sectionHead ? "1" : "2"} />
+        <TabsComponent className="react-tabs" tabs={tabs} id={!sectionHead ? "1" : "2"} />
       </Suspense>
+
+      {showSectionFooter && (
+        <footer className="tabsSection__footer">
+          {footerContent.blocks?.map((block, blockIndex) => {
+            return (
+              <Fragment key={blockIndex}>
+                {blockRenderers[block.blockType](block)}
+              </Fragment>
+            );
+          })}
+        </footer>
+      )}
     </section>
   );
 }
