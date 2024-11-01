e00fe50bb827ffb9837289d55fa6c76a8d119297
Bob Smith <reversetype@gmail.com>
2024-11-01 10:25:56 -0400

    add github workflow

--- sst.config.ts
+++ sst.config.ts
@@ -2,10 +2,21 @@
 
 export default $config({
   app(input) {
+    let domain;
+    if (input?.stage === "production") {
+      domain = "advance.appily.com";
+    } else if (input?.stage === "qa") {
+      domain = "qa.advance.appily.com";
+    } else {
+      domain = `${input.stage}.qa.advance.appily.com`;
+    }
+
+
     return {
       name: "advance-appily-com",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      domain,
     };
   },
   async run() {
