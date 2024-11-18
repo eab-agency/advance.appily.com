ded8232c5aa0c92134e63120f43990a196bd9982
Bob Smith <reversetype@gmail.com>
2024-11-18 13:08:06 -0500

    🔧 (package.json): resolve merge conflicts and clean up package.json ♻️ (page.tsx): remove unnecessary TypeScript ignore comments from BlogComponent
    The merge conflicts in package.json have been resolved, ensuring that the file reflects the correct dependencies and scripts for the project. Additionally, unnecessary TypeScript ignore comments have been removed from the BlogComponent, improving code clarity and maintainability.

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -57,7 +57,6 @@ const BlogComponent = async () => {
             <article key={index} className="post post__featured">
               <Link href={`blog/${catTitle}/${slug}`}>
                 <div className="post__content">
-                  {/*@ts-ignore */}
                   <PostHeader
                     title={title}
                     createdBy={createdBy}
@@ -88,7 +87,6 @@ const BlogComponent = async () => {
             return (
               <article key={index} className="post post__latest">
                 <Link href={`blog/${catTitle}/${slug}`}>
-                  {/*@ts-ignore */}
                   <PostHeader
                     title={title}
                     createdBy={createdBy}
