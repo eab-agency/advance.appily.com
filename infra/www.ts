ab6b12555fed7d827a319837ecb5b0bc18d8660d
Bob Smith <reversetype@gmail.com>
2024-12-02 13:15:09 -0500

    📝 (github.ts, www.ts, sst-env.d.ts, tsconfig.json): comment out unused code and update configuration for clarity and maintainability The changes involve commenting out unused code in `github.ts` to prevent execution while keeping it for future reference. In `www.ts`, the DNS and certificate settings are updated to ensure they are correctly configured for production. The `sst-env.d.ts` file is modified to define the `AppilyAdvanceSite` resource type, enhancing type safety. Lastly, the `tsconfig.json` file is updated to include `.next/types/**/*.ts` in the compilation process, ensuring that type definitions for Next.js are recognized. These changes improve code clarity, maintainability, and type safety across the project.

--- infra/www.ts
+++ infra/www.ts
@@ -6,7 +6,7 @@ export const www = new sst.aws.Nextjs("AppilyAdvanceSite", {
       name: domain,
       // redirects: $app.stage === 'production' ? [`www.${rootDomain}`] : [],
       // set dns to false if $app.stage === 'production'
-      // dns: $app.stage === 'production' ? false : undefined,
-      // cert: $app.stage === 'production' ? 'arn:aws:acm:us-east-1:631709423707:certificate/de28c699-14b0-42e8-a6e6-a452b868ca8e' : undefined
+      dns: $app.stage === 'production' ? false : undefined,
+      cert: $app.stage === 'production' ? 'arn:aws:acm:us-east-1:631709423707:certificate/11283a7c-1357-4d8b-bc6b-0d7d8b382044' : undefined
     },
 });
