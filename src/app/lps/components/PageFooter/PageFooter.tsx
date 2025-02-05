b052e5a6b1cb04909f8c74671f274a74e507a689
Andrei Olea <andrei@andreiolea.com>
2025-02-05 15:29:34 -0500

    🔨 refactor(Hero.tsx): update Hero component layout styles for better responsiveness
    🔧 fix(PageFooter.tsx): add privacyClick function to handle privacy link click event
    
    🎨 style(PageFooter.tsx): update footer links with proper URLs and add onClick event for Privacy Preferences link
    
    🚚 chore(page.tsx): update button label to 'Start the Free Health Care Career Quiz'

--- src/app/lps/components/PageFooter/PageFooter.tsx
+++ src/app/lps/components/PageFooter/PageFooter.tsx
@@ -1,9 +1,22 @@
+"use client";
 import { PreFooter } from '../PreFooter/PreFooter';
 
 const theDate = new Date();
 const year = theDate.getFullYear();
 
 export const PageFooter = () => {
+
+  const privacyClick = () => {
+    console.log('privacyClick');
+    if (
+      typeof window.OneTrust === "undefined" ||
+      typeof window.OneTrust.ToggleInfoDisplay !== "function"
+    ) {
+      return;
+    }
+    window.OneTrust.ToggleInfoDisplay();
+  };
+
   return (
     <>
     <PreFooter />
@@ -11,12 +24,13 @@ export const PageFooter = () => {
     <div className="container-wide flex flex-col gap-fluid-md">
       <nav>
         <ul className="flex flex-wrap justify-center gap-fluid-lg text-sm">
-          <li><a href="#" className="text-white" target="_blank">About Us</a></li>
-          <li><a href="#" className="text-white" target="_blank">Terms and Conditions</a></li>
-          <li><a href="#" className="text-white" target="_blank">California Privacy Notice</a></li>
-          <li><a href="#" className="text-white" target="_blank">Do Not Sell or Share My Personal Information</a></li>
-          <li><a href="#" className="text-white" target="_blank">Data Attribution</a></li>
-          <li><a href="#" className="text-white" target="_blank">Privacy Preferences</a></li>
+          <li><a href="https://www.appily.com/about?_gl=1*1eh98x6*_gcl_au*MTE3NTA0Mjc4LjE3Mzg3ODY2MzA.*_ga*MTk0NjgyNzk5NS4xNzM4Nzg2NjMw*_ga_LX9G5NB3RW*MTczODc4NjYzMC4xLjAuMTczODc4NjYzMC42MC4wLjA." className="text-white" target="_blank">About Us</a></li>
+          <li><a href="https://www.appily.com/terms-and-conditions?_gl=1*1eh98x6*_gcl_au*MTE3NTA0Mjc4LjE3Mzg3ODY2MzA.*_ga*MTk0NjgyNzk5NS4xNzM4Nzg2NjMw*_ga_LX9G5NB3RW*MTczODc4NjYzMC4xLjAuMTczODc4NjYzMC42MC4wLjA." className="text-white" target="_blank">Terms and Conditions</a></li>
+          <li><a href="https://www.appily.com/privacy-policy?_gl=1*1r8gjuk*_gcl_au*MTE3NTA0Mjc4LjE3Mzg3ODY2MzA.*_ga*MTk0NjgyNzk5NS4xNzM4Nzg2NjMw*_ga_LX9G5NB3RW*MTczODc4NjYzMC4xLjAuMTczODc4NjYzMC42MC4wLjA." className="text-white" target="_blank">Privacy Policy</a></li>
+          <li><a href="https://privacy.eab.com/appily-cs-tours-CA" className="text-white" target="_blank">California Privacy Notice</a></li>
+          <li><a href="https://www.appily.com/your-privacy-rights?_gl=1*1r8gjuk*_gcl_au*MTE3NTA0Mjc4LjE3Mzg3ODY2MzA.*_ga*MTk0NjgyNzk5NS4xNzM4Nzg2NjMw*_ga_LX9G5NB3RW*MTczODc4NjYzMC4xLjAuMTczODc4NjYzMC42MC4wLjA." className="text-white" target="_blank">Do Not Sell or Share My Personal Information</a></li>
+          <li><a href="https://www.appily.com/data?_gl=1*1r8gjuk*_gcl_au*MTE3NTA0Mjc4LjE3Mzg3ODY2MzA.*_ga*MTk0NjgyNzk5NS4xNzM4Nzg2NjMw*_ga_LX9G5NB3RW*MTczODc4NjYzMC4xLjAuMTczODc4NjYzMC42MC4wLjA." className="text-white" target="_blank">Data Attribution</a></li>
+          <li><a className="text-white cursor-pointer" target="_blank" onClick={privacyClick}>Privacy Preferences</a></li>
         </ul>
       </nav>
       <p className="text-xs text-white ">© {year} Appily Advance. All rights reserved.</p>
